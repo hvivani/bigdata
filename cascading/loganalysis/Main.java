585e9910577950babecb290e55c4181165446801
Hernan Vivani <hernan@vivani.com.ar>
2014-12-22 15:50:05 +0200

    Update Main.java

--- cascading/loganalysis/Main.java
+++ cascading/loganalysis/Main.java
@@ -27,13 +27,13 @@ public class Main {
  
         String inputPath 		= args[0]; //input will use default filesystem. HDFS or S3.
         String outputPath 	= args[1]; 
-       // String sortedTopTen = args[ 1 ] + "/top10/";
+        String sortedTopTen = args[ 1 ] + "/top10/";
         
         
         // sources and sinks
         Tap inTap 	= new Hfs(new TextLine(), inputPath); //input
         Tap outTap  = new Hfs(new TextDelimited(true, ";"), outputPath, SinkMode.REPLACE); //output
-        //Tap top10Tap = new Hfs( new TextDelimited(true, ";"), sortedTopTen, SinkMode.REPLACE);
+        Tap top10Tap = new Hfs( new TextDelimited(true, ";"), sortedTopTen, SinkMode.REPLACE);
         
         // Parse the line of input and break them into five fields
         RegexParser parser = new RegexParser(new Fields("ip", "time", "request", "response", "size"), 
@@ -41,14 +41,17 @@ public class Main {
         
         // "Each" pipe applies a Function or Filter Operation to each Tuple that passes through it.
         Pipe top10Pipe = new Each("top10Pipe", new Fields("line"), parser, Fields.RESULTS);
+        Pipe totalPipe = new Each("totalPipe", new Fields("line"), parser, Fields.RESULTS);
         
         // Grouping by 'ip' field: 
         // "GroupBy" manages one input Tuple stream and, groups the stream on selected fields in the tuple stream. 
         top10Pipe = new GroupBy(top10Pipe, new Fields("ip"));
+        totalPipe = new GroupBy(totalPipe, new Fields("ip"));
         
         // Aggregate each "ip" group using Count function:
         // "Every" pipe applies an Aggregator (like count, or sum) or Buffer (a sliding window) Operation to every group of Tuples that pass through it.
         top10Pipe = new Every(top10Pipe, Fields.GROUP, new Count(new Fields("IPcount")), Fields.ALL);
+        totalPipe = new Every(totalPipe, Fields.GROUP, new Count(new Fields("IPcount")), Fields.ALL);
         
         // After aggregation counter for each "ip," sort the counts. "true" is descending order
         Pipe top10CountPipe = new GroupBy(top10Pipe, new Fields("IPcount"), true);
@@ -60,8 +63,8 @@ public class Main {
         FlowDef flowDef = FlowDef.flowDef()
     		   .addSource(top10Pipe, inTap)
     		   //.addTailSink(top10Pipe, outTap) // comment to use sorted top 10
-    		   //.addTailSink(top10CountPipe, top10Tap) //uncomment to use sorted top 10
-    		   .addTailSink(top10CountPipe, outTap) //uncomment to use sorted top 10
+    		   .addTailSink(totalPipe, outTap) //uncomment to use sorted top 10
+    		   .addTailSink(top10CountPipe, top10Tap) //uncomment to use sorted top 10
     		   .setName("Top10IP");
         Properties properties = AppProps.appProps()
         		.setName("Top10IP")
