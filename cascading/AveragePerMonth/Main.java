d58c8e8e6bfb939493752d0dcb0e8cc779c845b7
Hernan Vivani <hernan@vivani.com.ar>
2015-01-04 00:01:39 +0200

    Update Main.java

--- cascading/AveragePerMonth/Main.java
+++ cascading/AveragePerMonth/Main.java
@@ -2,6 +2,7 @@ package com.amazonaws.vivanih.hadoop.cascading;
 
 import java.util.Properties;
 import java.util.Calendar;
+import java.text.SimpleDateFormat;
 
 import cascading.flow.Flow;
 import cascading.flow.FlowConnector;
@@ -16,12 +17,14 @@ import cascading.pipe.Each;
 import cascading.pipe.Every;
 import cascading.pipe.GroupBy;
 import cascading.pipe.Pipe;
+import cascading.pipe.assembly.Unique;
 import cascading.property.AppProps;
 import cascading.scheme.Scheme;
 import cascading.scheme.hadoop.TextDelimited;
 import cascading.tap.Tap;
 import cascading.tap.hadoop.Hfs;
 import cascading.tuple.Fields;
+
 
 
 
@@ -40,21 +43,25 @@ public class
 
     //scheme definition:
     Scheme inScheme = new TextDelimited( new Fields("LoadId", "MRP", "ServicerName", "CIR", "UPB", "LoanAge", "RMLM" , "ARMM", "MadurityDate", "MSA", "CLDS", "ModificationFlag", "ZBC", "ZBED", "RepurchaseIndicator"), "|");
-    //Scheme outScheme = new TextDelimited( new Fields( "MRP", "UPB" ) );
+    Scheme outScheme = new TextDelimited( new Fields( "year", "month", "UPBaverage"),"\t" ) ;
 
     // create source and sink taps
     Tap inTap = new Hfs( inScheme, inPath );
-    //Tap outTap = new Hfs( outScheme, outPath );
-    Tap outTap = new Hfs( new TextDelimited(true, ";"), outPath );
-    //Tap averageTap = new Hfs( outSchemeAverage, averagePath );
+    Tap outTap = new Hfs( outScheme, outPath );
+    //Tap outTap = new Hfs( new TextDelimited(true, "\t"), outPath );
 
-    //Parsing the date:
+    //Parsing the date. (0 is January)
     DateParser dateParser= new DateParser(new Fields("month", "day", "year"), new int[] { Calendar.MONTH, Calendar.DAY_OF_MONTH, Calendar.YEAR }, "MM/dd/yyyy");
     //DateParser dateParser= new DateParser(new Fields("month", "day", "year"), "MM/dd/yyyy");
     //parse pipe will parse the MRP field into month, day, year. /with Fields.ALL I get original fields + new fields. With Fields.RESULTS, I get only the date parsed.
     Pipe parsePipe = new Each("parsePipe", new Fields("MRP"), dateParser , Fields.ALL);
+
+    //Pipe to filter duplicates. It is like select distinct. It is like using combiners.
+    //http://docs.cascading.org/cascading/2.2/javadoc/cascading/pipe/assembly/Unique.html
+    Pipe uniquePipe = new Unique("unique", parsePipe, new Fields("LoadId","MRP"));
+
     // aggregate by year, month. input is previous parsePipe
-    Pipe averagePipe = new GroupBy( "averagePipe", parsePipe, new Fields("year", "month" ));
+    Pipe averagePipe = new GroupBy( "averagePipe", uniquePipe, new Fields("year", "month" ));
     // average each aggregation
     averagePipe = new Every(averagePipe, new Fields("UPB"), new Average(new Fields("UPBaverage")), Fields.ALL );
 
@@ -62,7 +69,9 @@ public class
     // connect the taps, pipes, etc., into a flow
     FlowDef flowDef = FlowDef.flowDef()
     .addSource( averagePipe, inTap )
+    //.addSource( parsePipe, inTap )
     .addTailSink(averagePipe, outTap );
+    //.addTailSink(parsePipe, outTap );
     // run the flow
     flowConnector.connect( flowDef ).complete();
     }
