c0ddbdf5e9508f3d1b0e1b0a64b53cadb1517b2e
Hernan Vivani <hernan@vivani.com.ar>
2015-01-03 20:44:08 +0200

    Update Main.java

--- cascading/AveragePerMonth/Main.java
+++ cascading/AveragePerMonth/Main.java
@@ -1,13 +1,17 @@
 package com.amazonaws.vivanih.hadoop.cascading;
 
 import java.util.Properties;
+import java.util.Calendar;
 
 import cascading.flow.Flow;
 import cascading.flow.FlowConnector;
 import cascading.flow.FlowDef;
 import cascading.flow.hadoop.HadoopFlowConnector;
 import cascading.operation.aggregator.Count;
+import cascading.operation.aggregator.Average;
 import cascading.operation.regex.RegexSplitGenerator;
+import cascading.operation.text.DateParser;
+import cascading.tap.SinkMode;
 import cascading.pipe.Each;
 import cascading.pipe.Every;
 import cascading.pipe.GroupBy;
@@ -18,6 +22,7 @@ import cascading.scheme.hadoop.TextDelimited;
 import cascading.tap.Tap;
 import cascading.tap.hadoop.Hfs;
 import cascading.tuple.Fields;
+
 
 
 public class
@@ -35,19 +40,29 @@ public class
 
     //scheme definition:
     Scheme inScheme = new TextDelimited( new Fields("LoadId", "MRP", "ServicerName", "CIR", "UPB", "LoanAge", "RMLM" , "ARMM", "MadurityDate", "MSA", "CLDS", "ModificationFlag", "ZBC", "ZBED", "RepurchaseIndicator"), "|");
-    Scheme outScheme = new TextDelimited( new Fields( "MRP", "UPB" ) );
+    //Scheme outScheme = new TextDelimited( new Fields( "MRP", "UPB" ) );
 
     // create source and sink taps
     Tap inTap = new Hfs( inScheme, inPath );
-    Tap outTap = new Hfs( outScheme, outPath );
+    //Tap outTap = new Hfs( outScheme, outPath );
+    Tap outTap = new Hfs( new TextDelimited(true, ";"), outPath );
+    //Tap averageTap = new Hfs( outSchemeAverage, averagePath );
+
+    //Parsing the date:
+    DateParser dateParser= new DateParser(new Fields("month", "day", "year"), new int[] { Calendar.MONTH, Calendar.DAY_OF_MONTH, Calendar.YEAR }, "MM/dd/yyyy");
+    //DateParser dateParser= new DateParser(new Fields("month", "day", "year"), "MM/dd/yyyy");
+    //parse pipe will parse the MRP field into month, day, year. /with Fields.ALL I get original fields + new fields. With Fields.RESULTS, I get only the date parsed.
+    Pipe parsePipe = new Each("parsePipe", new Fields("MRP"), dateParser , Fields.ALL);
+    // aggregate by year, month. input is previous parsePipe
+    Pipe averagePipe = new GroupBy( "averagePipe", parsePipe, new Fields("year", "month" ));
+    // average each aggregation
+    averagePipe = new Every(averagePipe, new Fields("UPB"), new Average(new Fields("UPBaverage")), Fields.ALL );
 
 
-    // specify a pipe to connect the taps
-    Pipe copyPipe = new Pipe( "copy" );
     // connect the taps, pipes, etc., into a flow
     FlowDef flowDef = FlowDef.flowDef()
-    .addSource( copyPipe, inTap )
-    .addTailSink( copyPipe, outTap );
+    .addSource( averagePipe, inTap )
+    .addTailSink(averagePipe, outTap );
     // run the flow
     flowConnector.connect( flowDef ).complete();
     }
