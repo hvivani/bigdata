247fb0cba817712db8e6fd5106c46ea6db03106c
Hernan Vivani <hernan@vivani.com.ar>
2015-04-16 13:49:05 +0200

    Update CommonCrawlIndex.java

--- cascading/commoncrawl.cascading.elasticsearch/src/main/java/com/amazonaws/bigdatablog/indexcommoncrawl/CommonCrawlIndex.java
+++ cascading/commoncrawl.cascading.elasticsearch/src/main/java/com/amazonaws/bigdatablog/indexcommoncrawl/CommonCrawlIndex.java
@@ -4,7 +4,8 @@ import cascading.flow.FlowDef;
 import cascading.operation.regex.RegexGenerator;
 import cascading.pipe.Each;
 import cascading.pipe.Pipe;
-import cascading.scheme.local.TextLine;
+//import cascading.scheme.local.TextLine;
+import cascading.scheme.hadoop.TextLine;
 import cascading.tap.Tap;
 import cascading.tap.local.FileTap;
 import cascading.tuple.Fields;
@@ -15,8 +16,8 @@ public class CommonCrawlIndex {
 
     public static FlowDef buildFlowDef(Properties properties){
         // create the Cascading "source" (input) tap to read the commonCrawl WAT file(s)
-        Tap<?, ?, ?> source = new FileTap(new TextLine(new Fields("line"))
-                ,properties.getProperty("inPath"));
+        //Tap<?, ?, ?> source = new FileTap(new TextLine(new Fields("line")) ,properties.getProperty("inPath"));
+        Tap<?, ?, ?>  source = new Hfs(new TextLine(new Fields("line")),properties.getProperty("inPath"));
 
         // create the "sink" (output) tap that will export the data to Elasticsearch
         Tap sink = new EsTap(properties.getProperty("es.target.index"));
