242262dc1e93d7ec1ffd1c262be75357b0aeb63f
Hernan Vivani <hernan@vivani.com.ar>
2015-04-18 20:27:21 +0200

    Update CommonCrawlIndexTest.java
    Updating to allow testing in DISTRIBUTED mode.

--- cascading/commoncrawl.cascading.elasticsearch/src/test/java/com/amaonaws/bigdatablog/indexcommoncrawl/CommonCrawlIndexTest.java
+++ cascading/commoncrawl.cascading.elasticsearch/src/test/java/com/amaonaws/bigdatablog/indexcommoncrawl/CommonCrawlIndexTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import cascading.flow.FlowDef;
 import cascading.flow.local.LocalFlowConnector;
+import java.io.IOException;
 import java.util.Properties;
 import com.amazonaws.bigdatablog.indexcommoncrawl.ConfigReader;
 import com.amazonaws.bigdatablog.indexcommoncrawl.CommonCrawlIndex;
@@ -18,11 +19,18 @@ public class CommonCrawlIndexTest {
 
     @Test
     public void testMain() throws Exception {
-        Properties properties = new ConfigReader().renderProperties(CommonCrawlIndexTest.class);
-        FlowDef flowDef = CommonCrawlIndex.buildFlowDef(properties);
-        //Using cascading Local connector to exclude Hadoop and just test the logic
-        new LocalFlowConnector(properties).connect(flowDef).complete();
+
+        Properties properties = null;
+        try {
+            properties = new ConfigReader().renderProperties(CommonCrawlIndexTest.class);
+            if (properties.getProperty("platform").toString().compareTo("LOCAL")==0){
+                FlowDef flowDef = CommonCrawlIndex.buildFlowDef(properties);
+                //Using cascading Local connector to exclude Hadoop and just test the logic
+                new LocalFlowConnector(properties).connect(flowDef).complete();
+            }
+        } catch (IOException e) {
+            System.out.println("Could not read your config.properties file");e.printStackTrace();
+        }
     }
-
-
 }
+
