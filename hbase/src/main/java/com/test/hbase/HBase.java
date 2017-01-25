3500c528b5d17b37a80a9f1388fd1cca41dcc520
hvivani <hernan@vivani.com.ar>
2017-01-25 23:30:56 +0000

    Adding HBase MR[B

--- hbase/src/main/java/com/test/hbase/HBase.java
+++ hbase/src/main/java/com/test/hbase/HBase.java
@@ -15,47 +15,15 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.hbase.mapreduce.*;
 import org.apache.hadoop.hbase.io.*;
-import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
 import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
 
 
 public class HBase {
 
-  /*public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-      private final static IntWritable one = new IntWritable(1);
-      private Text word = new Text();
 
-      public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-         String line = value.toString();
-         StringTokenizer tokenizer = new StringTokenizer(line);
-         while (tokenizer.hasMoreTokens()) {
-            word.set(tokenizer.nextToken());
-            context.write(word, one);
-         }
-      }
-   }*/
-
-  /* public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-      public void reduce(Text key, Iterable<IntWritable> values, Context context)
-	 throws IOException, InterruptedException {
-            int sum = 0;
-	    for (IntWritable val : values) {
-	       sum += val.get();
-            }
-	    context.write(key, new IntWritable(sum));
-	 }
-      }*/
-
-/*   public static class MyHBaseMapper extends TableMapper<Text, Text> {
-
-	     public void map(ImmutableBytesWritable row, Result value, Context context) throws InterruptedException, IOException {
-		         // process data for the row from the Result instance.
-	     }
-   }*/
 
 public static class MyHBaseMapper extends TableMapper<ImmutableBytesWritable, Put>  {
 	public void map(ImmutableBytesWritable row, Result value, Context context) throws IOException, InterruptedException {
@@ -103,19 +71,6 @@ public static class MyHBaseMapper extends TableMapper<ImmutableBytesWritable, Pu
 	  throw new IOException("error with job!");
        }
 
-      // job.setOutputKeyClass(Text.class);
-      // job.setOutputValueClass(IntWritable.class);
-
-      // job.setMapperClass(Map.class);
-      // job.setReducerClass(Reduce.class);
-
-      // job.setInputFormatClass(TextInputFormat.class);
-      // job.setOutputFormatClass(TextOutputFormat.class);
-
-      // FileInputFormat.addInputPath(job, new Path(args[0]));
-      // FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-       //job.waitForCompletion(true);
     }
 }
 
