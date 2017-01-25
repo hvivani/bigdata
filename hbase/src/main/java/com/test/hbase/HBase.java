593c66e5c3a1ac78e534edf4d90745a7f844f131
hvivani <hernan@vivani.com.ar>
2017-01-25 23:01:49 +0000

    Adding HBase MR

--- hbase/src/main/java/com/test/hbase/HBase.java
+++ hbase/src/main/java/com/test/hbase/HBase.java
@@ -19,10 +19,12 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.hbase.mapreduce.*;
 import org.apache.hadoop.hbase.io.*;
 import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
+import org.apache.hadoop.hbase.KeyValue;
 
 
 public class HBase {
-   public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+  /*public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
       private final static IntWritable one = new IntWritable(1);
       private Text word = new Text();
 
@@ -34,9 +36,9 @@ public class HBase {
             context.write(word, one);
          }
       }
-   }
+   }*/
 
-   public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+  /* public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 
       public void reduce(Text key, Iterable<IntWritable> values, Context context)
 	 throws IOException, InterruptedException {
@@ -46,15 +48,31 @@ public class HBase {
             }
 	    context.write(key, new IntWritable(sum));
 	 }
-      }
+      }*/
 
-   public static class MyHBaseMapper extends TableMapper<Text, Text> {
+/*   public static class MyHBaseMapper extends TableMapper<Text, Text> {
 
 	     public void map(ImmutableBytesWritable row, Result value, Context context) throws InterruptedException, IOException {
 		         // process data for the row from the Result instance.
 	     }
-   }
+   }*/
 
+public static class MyHBaseMapper extends TableMapper<ImmutableBytesWritable, Put>  {
+	public void map(ImmutableBytesWritable row, Result value, Context context) throws IOException, InterruptedException {
+		// this example is just copying the data from the source table...
+ 		context.write(row, resultToPut(row,value));
+ 	}
+			
+        private static Put resultToPut(ImmutableBytesWritable key, Result result) throws IOException {
+ 	 	Put put = new Put(key.get());
+		for (KeyValue kv : result.raw()) {
+		put.add(kv);
+		}
+	 return put;
+	}
+}
+						  		 									   	    
+					    		   	  	  		 									   	    
     public static void main(String[] args) throws Exception {
        Configuration conf = new Configuration();
        Job job = new Job(conf, "hbase");
@@ -63,6 +81,7 @@ public class HBase {
        Scan scan = new Scan();
        scan.setCaching(500);        // 1 is the default in Scan, which will be bad for MapReduce jobs
        scan.setCacheBlocks(false);  // don't set to true for MR jobs
+
        TableMapReduceUtil.initTableMapperJob(
 	  "test",        // input HBase table name
           scan,             // Scan instance to control CF and attribute selection
@@ -70,7 +89,14 @@ public class HBase {
           null,             // mapper output key
 	  null,             // mapper output value
        job);
-       job.setOutputFormatClass(NullOutputFormat.class);   // because we aren't emitting anything from mapper
+
+       TableMapReduceUtil.initTableReducerJob(
+	"OutPutTable",      // output table
+	null,             // reducer class
+       job);
+       
+       job.setNumReduceTasks(0);
+       //job.setOutputFormatClass(NullOutputFormat.class);   // because we aren't emitting anything from mapper
 
        boolean b = job.waitForCompletion(true);
        if (!b) {
